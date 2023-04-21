ad69cad65fb5346ea8cf54554cbfb1c517a331c7
Aster <aster@vers.site>
2023-04-21 09:31:16 +0000

    🧨 Remove survivor in random kill game

--- projects/josephus-problem/src/random/mod.rs
+++ projects/josephus-problem/src/random/mod.rs
@@ -1,13 +1,31 @@
 use rand::prelude::SmallRng;
-use rand::Rng;
+use rand::{Rng, SeedableRng};
 
+#[derive(Copy, Clone, Debug)]
 pub struct RandomKill {
     kill_rate: f64,
     partners: usize,
     seed: u64,
 }
 
-struct State {
+impl RandomKill {
+    pub fn new(partners: usize, kill_rate: f64) -> Self {
+        RandomKill {
+            kill_rate,
+            partners,
+            seed: rand::random(),
+        }
+    }
+    pub fn set_seed(&mut self, seed: u64) {
+        self.seed = seed;
+    }
+    pub fn with_seed(mut self, seed: u64) -> Self {
+        self.set_seed(seed);
+        self
+    }
+}
+
+pub struct RandomKillState {
     rng: SmallRng,
     kill_rate: f64,
     partners: Vec<usize>,
@@ -17,14 +35,37 @@ struct State {
 
 impl IntoIterator for RandomKill {
     type Item = usize;
-    type IntoIter = State;
+    type IntoIter = RandomKillState;
 
     fn into_iter(self) -> Self::IntoIter {
-        todo!()
+        let mut rng = SmallRng::seed_from_u64(self.seed);
+        let mut partners = Vec::with_capacity(self.partners);
+        for i in 0..self.partners {
+            partners.push(i);
+        }
+        RandomKillState {
+            rng,
+            kill_rate: self.kill_rate,
+            partners,
+            killer_index: 0,
+            killer_steps: 0,
+        }
     }
 }
 
-impl Iterator for State {
+impl RandomKillState {
+    pub fn survivor(&self) -> usize {
+        assert_eq!(self.partners.len(), 1, "survivor() called before iteration finished");
+        unsafe {
+            *self.partners.get_unchecked(0)
+        }
+    }
+    pub fn killer_steps(&self) -> usize {
+        self.killer_steps
+    }
+}
+
+impl Iterator for RandomKillState {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
--- projects/josephus-problem/tests/main.rs
+++ projects/josephus-problem/tests/main.rs
@@ -1,4 +1,16 @@
+use josephus::RandomKill;
+
 #[test]
 fn ready() {
     println!("it works!")
+}
+
+
+#[test]
+fn test() {
+    let mut game = RandomKill::new(10, 0.5).into_iter();
+    for (count, victim) in game {
+        println!("victim-{}: {}", count + 1, victim);
+    }
+    println!("survivor: {}", game.survivor());
 }
